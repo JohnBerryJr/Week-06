c3f7de5679f860184b77efae8ee3b15581b6c283
John Berry <johnwberryjr@gmail.com>
2022-09-07 17:14:57 -0400

    Added Assignment Parameters

---
+++ script.js
@@ -0,0 +1,5 @@
+// Deal 26 Cards to two Players from a Deck. 
+// Iterate through the turns where each Player plays a Card
+// The Player who played the higher card is awarded a point
+// Ties result in zero points for both Players
+// After all cards have been played, display the score and declare the winner.
