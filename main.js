e988f4f9a44233d85a0106561abef3c490a2f31a
John Berry <johnwberryjr@gmail.com>
2022-09-08 23:17:32 -0400

    Created deck.js and styles.css

--- main.js
+++
@@ -1,7 +0,0 @@
-// Deal 26 Cards to two Players from a Deck. 
-// Iterate through the turns where each Player plays a Card
-// The Player who played the higher card is awarded a point
-// Ties result in zero points for both Players
-// After all cards have been played, display the score and declare the winner.
-
-
--- script.js
+++ script.js
@@ -1,6 +1,108 @@
-// Deal 26 Cards to two Players from a Deck. 
-// Iterate through the turns where each Player plays a Card
-// The Player who played the higher card is awarded a point
-// Ties result in zero points for both Players
-// After all cards have been played, display the score and declare the winner.
+import Deck from "./deck";
 
+const CARD_VALUE_MAP = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14,
+}
+
+const computerCardSlot = document.querySelector('.computer-card-slot')
+const playerCardSlot = document.querySelector('.player-card-slot')
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.computer-deck')
+const text = document.querySelector('.text')
+
+let playerDeck, computerDeck, inRound, stop
+
+document.addEventListener('click', () => {
+    if (stop) {
+        startGame()
+        return
+    }
+
+    if (inRound) {
+        cleanBeforeRound() 
+    } else {
+        flipCards()
+    }
+})
+
+
+startGame()
+function startGame() {
+    const decl = new Deck()
+    deck.shuffle()
+
+    const deckMidpoint = MAth.ceil(deck.numberOfCards / 2)
+    playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+    computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+    inRound = false
+    stop = false
+    cleanBeforeRound() 
+}
+
+function cleanBeforeRound() {
+    inRound = false
+    computerCardSlot.innerHTML = ''
+    playerCardSlot.innterHTML = ''
+    text.innerText = ''
+
+    updateDeckCount()
+}
+
+function flipCards() {
+    inRound = true
+
+    const playerCard = playerDeck.pop()
+    const compiuterCard = computerDeck.pop()
+
+    playerCardSlot.appendChild(playerCard.getHTML())
+    computerCardSlot.appendChild(computerCard.getHTML())
+
+    updateDeckCount()
+
+    if (isRoundWinner(playerCard, computerCardSlot)) {
+        Text.innerHTML = 'Win'
+        playerDeck.push(playerCards)
+        playerDeck.push(computerCard)
+    } else if (isRoundWinner(computerCard, playerCard)) {
+        Text.innerHTML = 'Lose'
+        playerDeck.push(playerCard);
+        playerDeck.push(computerCard);
+    } else {
+        text.innerHTML = 'Draw'
+        playerDeck.push(playerCard)
+        computerDeck.push(computerCard)
+    }
+
+    if (isGameOver(playerDeck)) {
+        text.innerText = 'You Loose!'
+        stop = true
+    } else if {isGameOver(computerDeck)
+        
+    }
+} 
+
+function updateDeckCount() {
+    computerDeckElement.innerText = computerDeck.numberOfCards
+    playerDeckElement.innerText = computerDeck.numberOfCards
+}
+
+function isRoundWinner(cardOne, cardTwo) {
+    return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo{
+    }
+}
+
+function isGameOver(deck) {
+    return deck.numberOfCards === 0
+})
